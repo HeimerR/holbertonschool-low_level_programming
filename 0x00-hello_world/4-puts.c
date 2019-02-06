6c0e45a6c0c4d63049644816f4bf35a99bb21462
Heimer <732@holbertonschool.com>
2019-02-06 19:42:09 +0000

    q4 arreglo vi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 int main(void)
 {
-char text[60];
-strcpy(text, "\"Programming is like building a multilingual puzzle");
-puts(text);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
