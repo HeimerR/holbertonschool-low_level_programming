f4ba681e4c78db070db3f579ab7a2f915f9bb556
Heimer <732@holberton.com>
2019-02-07 20:39:29 +0000

    7q

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+	putchar(letter);
+	}
+printf("\n");
+return (0);
+}
