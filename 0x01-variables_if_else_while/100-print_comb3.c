1e801a227ff6ec5a50ab135bd4d5d359db6d4e2f
Heimer <732@holberton.com>
2019-02-07 21:57:12 +0000

    11q

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int number;
+int number2;
+	for (number = '0'; number <= '9'; number++)
+	{
+		for (number2 = '0'; number2 <= '9'; number2++)
+		{
+		if (number < number2 && number != number2)
+		{
+		putchar(number);
+		putchar(number2);
+		if (number == '8' && number2 == '9')
+		{
+		}
+		else
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+		}
+	}
+printf("\n");
+return (0);
+}
