975779f1b9930c868fbbc3375ef737d9f794e224
Heimer <732@holberton.com>
2019-02-11 23:43:28 +0000

    q8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+void jack_bauer(void)
+{
+ 
+  int min;
+  int hour;
+  for (hour = 0; hour < 24; hour++)
+    {
+      for (min = 0; min < 60; min++)
+	{
+	  _putchar(hour/10 + '0');
+	  _putchar(hour%10 + '0');
+	  _putchar(':');
+	  _putchar(min/10 + '0'); 
+	  _putchar(min%10 + '0');
+	  _putchar('\n');
+        }    
+    }
+
+}
