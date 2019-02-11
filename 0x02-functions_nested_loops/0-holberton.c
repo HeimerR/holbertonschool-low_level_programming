5feedd645ed74f53d0d22ad9d635e962e65da5bc
Heimer <732@holberton.com>
2019-02-11 21:03:55 +0000

    q0.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+int main(void)
+{
+  char out[] = "Holberton";
+  int n; 
+  for (n = 0; n <= 8; n++)
+      {
+	_putchar(out[n]);
+      }
+    _putchar('\n'); 
+return(0);
+}
