ea0eb4f8ffbffd9a8b17056f3716baa287915ec1
Heimer <732@holberton.com>
2019-02-12 14:57:57 +0000

    q5 betty corrections

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,26 @@
 #include "holberton.h"
+/**
+  * print_sign - prints the sign of a number.
+  * @n: interger input
+  * Return: boolean value
+**/
 int print_sign(int n)
 {
-
- int value = 0;
- if (n > 0)
-   {
-     value = 1;
-     _putchar('+');
-   }
- if (n == 0)
-   {
-     value = 0;
-     _putchar('0');
-   }
- if (n < 0) 
-   {
-     value = -1;
-     _putchar('-');
-   } 
- return(value);
+int value = 0;
+	if (n > 0)
+	{
+	value = 1;
+	_putchar('+');
+	}
+	if (n == 0)
+	{
+	value = 0;
+	_putchar('0');
+	}
+	if (n < 0)
+	{
+	value = -1;
+	_putchar('-');
+	}
+return (value);
 }
