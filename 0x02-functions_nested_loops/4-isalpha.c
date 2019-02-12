2a275e19f031b614b15423e5428395227520f17e
Heimer <732@holberton.com>
2019-02-12 14:53:18 +0000

    q4 betty corrections

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,26 @@
 #include "holberton.h"
+/**
+  * _isalpha - checks alpahabetic characters
+  * @c: input character
+  * Return: boolean value
+**/
 int _isalpha(int c)
 {
- char letter;
- int value = 0;
-     for (letter = 'a'; letter <= 'z'; letter++)
-       {
-	 if ( letter == c )
-	   {
-	   value = 1;
-	   }
-       }
-     for (letter = 'A'; letter <= 'Z'; letter++)
-       {
-         if ( letter == c )
-           {
-	     value = 1;
-           }
-       }
- return(value);
+char letter;
+int value = 0;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == c)
+		{
+		value = 1;
+		}
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		if (letter == c)
+		{
+			value = 1;
+		}
+	}
+return (value);
 }
