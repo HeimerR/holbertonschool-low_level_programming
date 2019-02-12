6f258062e58102b7a3ea664d25660f5e960d18b6
Heimer <732@holberton.com>
2019-02-12 15:55:06 +0000

    q11 review

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,20 @@
 void print_to_98(int n)
 {
 int i;
+
 	if (n < 98)
 	{
-	for (i = n; i < 98; i++)
-	{
-	printf("%d,  ", i);
-	}
+		for (i = n; i < 98; i++)
+		{
+		printf("%d,  ", i);
+		}
 	}
 	if (n > 98)
 	{
-	for (i = n; i > 98; i--)
-	{
-	printf("%d, ", i);
-	}
+		for (i = n; i > 98; i--)
+		{
+		printf("%d, ", i);
+		}
 	}
 	printf("98\n");
 }
