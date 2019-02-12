7b4685a406049a00c3edd57a0374cd5c15c513eb
Heimer <732@holberton.com>
2019-02-12 18:01:50 +0000

    este si

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,40 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
- * print_to_98 - show the absolute value of a number
- * @n: number to check
- *
- */
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: input , start point, integer
+ * Return: no return
+ **/
 void print_to_98(int n)
 {
-int i;
-
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-printf("%d", i);
-if (i < 98)
-{
-printf(", ");
-}
-}
-}
-else
-{
-for (i = n; i >= 98; i--)
-{
-printf("%d", i);
-if (i > 98)
-{
-printf(", ");
-}
-}
-}
+int num;
+	if (n <= 98)
+	{
+	for (num = n; num <= 98; num++)
+	{
+	if (num == 98)
+	{
+	printf("%d", num);
+	}
+	else
+	{
+	printf("%d, ", num);
+	}
+	}
+	}
+	if (n > 98)
+	{
+	for (num = n; num >= 98; num--)
+	{
+	if (num == 98)
+	{
+	printf("%d", num);
+	}
+	else
+	{
+	printf("%d, ", num);
+	}
+	}
+	}
 printf("\n");
 }
