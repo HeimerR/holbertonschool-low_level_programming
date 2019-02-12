7f8841d0484bed2692b53eb03c5b4d3407876ee2
Heimer <732@holberton.com>
2019-02-12 15:34:22 +0000

    q10 betty corrections

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
+/**
+  * add - adds two integers and returns the result
+  * @n1: input integer 1
+  * @n2: input integer 2
+  * Return: addition
+**/
 int add(int n1, int n2)
 {
-  return (n1+n2);
+return (n1 + n2);
 }
