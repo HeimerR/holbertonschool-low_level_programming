9379774875de80027f446cea0575a5444d28a651
Heimer <732@holberton.com>
2019-02-12 13:52:41 +0000

    q0 betty correction

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 int main(void)
 {
-  char out[] = "Holberton";
-  int n; 
-  for (n = 0; n <= 8; n++)
-      {
+char out[] = "Holberton";
+int n;
+	for (n = 0; n <= 8; n++)
+	{
 	_putchar(out[n]);
-      }
-    _putchar('\n'); 
-return(0);
+	}
+	_putchar('\n');
+return (0);
 }
