3c8e6a22b76c8c7bc3c4da8e60eee91d56455add
Heimer <732@holberton.com>
2019-02-12 14:44:42 +0000

    q3 betty correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,19 @@
-#include "holberton.h"
+#include "holbertion.h"
+/**
+  * _islower - checks lower case characters
+  * @c: input character
+  * Return: boolean value
+**/
 int _islower(int c)
 {
-  char letter;
- int value = 0;
-     for (letter = 'a'; letter <= 'z'; letter++)
-       {
-	 if ( letter == c )
-	   {
-	   value = 1;
-	   }
-       }
- 
- return(value);
+char letter;
+int value = 0;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == c)
+		{
+		value = 1;
+		}
+	}
+return (value);
 }
