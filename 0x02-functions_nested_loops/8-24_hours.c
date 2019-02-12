14f9063337189279945d804e68781973599d7589
Heimer <732@holberton.com>
2019-02-12 15:21:59 +0000

    q8 betty corrections

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,23 @@
 #include "holberton.h"
+/**
+  * jack_bauer - prints every minute of the day
+  *
+  * Return: no return
+**/
 void jack_bauer(void)
 {
- 
-  int min;
-  int hour;
-  for (hour = 0; hour < 24; hour++)
-    {
-      for (min = 0; min < 60; min++)
+int min;
+int hour;
+	for (hour = 0; hour < 24; hour++)
 	{
-	  _putchar(hour/10 + '0');
-	  _putchar(hour%10 + '0');
-	  _putchar(':');
-	  _putchar(min/10 + '0'); 
-	  _putchar(min%10 + '0');
-	  _putchar('\n');
-        }    
-    }
-
+		for (min = 0; min < 60; min++)
+		{
+		_putchar(hour / 10 + '0');
+		_putchar(hour % 10 + '0');
+		_putchar(':');
+		_putchar(min / 10 + '0');
+		_putchar(min % 10 + '0');
+		_putchar('\n');
+		}
+	}
 }
