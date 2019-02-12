8d952ec7b9aa06398e55b67d09314d13482045c6
Heimer <732@holberton.com>
2019-02-12 14:14:37 +0000

    1q betty correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
-int print_alphabet()
+/**
+  * print_alphabet - prints the alphabeth
+  *
+  * Return: 0
+**/
+void print_alphabet(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-  {
-  _putchar(letter);
-  }
- _putchar('\n');
- return(0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+return (0);
 }
