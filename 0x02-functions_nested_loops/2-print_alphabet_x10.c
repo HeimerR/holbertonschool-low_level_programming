fc7b16c57721007dc3ab74ed697aa8dc5f48c3b9
Heimer <732@holberton.com>
2019-02-12 14:25:15 +0000

    q2 betty correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,20 @@
 #include "holberton.h"
-int print_alphabet_x10()
+/**
+  * print_alphabet_x10 - prints alphabet lower cases
+  *
+  * Return: 0
+**/
+int print_alphabet_x10(void)
 {
 char letter;
- int n;
- for (n = 0; n <= 9; n++)
-   {
-     for (letter = 'a'; letter <= 'z'; letter++)
-       {
-        _putchar(letter);
-       }
-       _putchar('\n');
-   }
- return(0);
+int n;
+	for (n = 0; n <= 9; n++)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		_putchar(letter);
+		}
+		_putchar('\n');
+		}
+return (0);
 }
