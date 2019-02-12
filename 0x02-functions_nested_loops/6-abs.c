9cfd80188297491915e554ca78028e8560f64689
Heimer <732@holberton.com>
2019-02-12 15:01:22 +0000

    q6 betty correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
+/**
+  * _abs - computes the absolute value of an integer.
+  * @n: input integer
+  * Return: absolute value
+**/
 int _abs(int n)
 {
-  int abs;
-  if (n >= 0)
-    {
-      abs = n;
-    }
-  else
-    {
-      abs = -n;
-    }
- return(abs);
+int abs;
+	if (n >= 0)
+	{
+	abs = n;
+	}
+	else
+	{
+	abs = -n;
+	}
+return (abs);
 }
