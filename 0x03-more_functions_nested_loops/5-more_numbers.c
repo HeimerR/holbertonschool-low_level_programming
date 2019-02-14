293b3f07d13c422605dd32cc8fac32c6949e9593
Heimer <732@holberton.com>
2019-02-14 18:13:28 +0000

    q5

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+  *  more_numbers  - prints 10 time numbers 0-9
+  * Return: no return
+**/
+void more_numbers(void)
+{
+	int i;
+	int n;
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+		if (n > 9)
+		{
+		_putchar(n / 10 + '0');
+		}
+		_putchar(n % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
+
