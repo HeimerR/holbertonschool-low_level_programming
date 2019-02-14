bfb4bb163fe21230a04a4cc334ecca23854d8485
Heimer <732@holberton.com>
2019-02-14 17:47:49 +0000

    q4 correction

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-  * print_numbers - prints numbers 0-9 without 2,4
+  * print_most_numbers - prints numbers 0-9 without 2,4
   * Return: no return
 **/
 void print_most_numbers(void)
@@ -8,7 +8,7 @@ void print_most_numbers(void)
 int n;
 for (n = 0; n < 10; n++)
 {
-	if (n != 2 && n != 4) 
+	if (n != 2 && n != 4)
 	{
 	_putchar(n + '0');
 	}
