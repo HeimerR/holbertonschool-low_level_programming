83446fd5997b3c4d3fbdfd5d0d67bdeca45194b6
Heimer <732@holberton.com>
2019-02-14 20:18:21 +0000

    q9betty

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 /**
   * main - prints the numbers from 1 to 100
   * followed by a new line. But for multiples of 3
-  * print Fizz instead of the number 
+  * print Fizz instead of the number
   * and for the multiples of five print Buzz
   * For numbers which are multiples of both FizzBuzz.
   * Return: 0
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int i, c;
+
 	for (i = 1; i < 100; i++)
 	{
 	if ((i % 3) == 0)
