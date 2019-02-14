d1e68dd01902020228db60ff604fd9faa82255cc
Heimer <732@holberton.com>
2019-02-14 18:32:29 +0000

    q6 corrections

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
   * print_line - prints a line
-  * @n: lenght of line 
+  * @n: lenght of line
   * Return: no return
 **/
 void print_line(int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 	_putchar('_');
