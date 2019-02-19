512acd1ae465e7e22cef686e78a448865fb201d7
Heimer <732@holberton.com>
2019-02-19 17:40:31 +0000

    q8correction

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -8,9 +8,15 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < (n - 1); i++)
+for (i = 0; i < n; i++)
+{
+if ( i == (n - 1))
+{
+printf("%d\n", a[i]);
+}
+else
 {
 printf("%d, ", a[i]);
 }
-printf("%d\n", a[n - 1]);
+}
 }
