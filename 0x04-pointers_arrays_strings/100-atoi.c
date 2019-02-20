b7ed632221e59e4e0ae789f9281dbf8a5c14eed8
Heimer <732@holberton.com>
2019-02-20 03:16:47 +0000

    q10 ajustment

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -6,36 +6,41 @@
 **/
 int _atoi(char *s)
 {
-int size = 0, negatives = 0, p = 1;
-int j, number = 0, start = 0, end;
-while (s[size] != '\0')
-{
-size++;
-}
-while (s[start] < 48 || s[start] > 57)
-{
-start++;
-	if (start == size)
+	int size = 0, negatives = 0, p = 1, j, start = 0, end;
+	unsigned int number = 0;
+	int number2 = 0;
+	while (s[size] != '\0')
 	{
-	return (number);
+	size++;
 	}
-}
-end = start;
-while (s[end + 1] > 47 && s[end + 1] < 58)
-{
-end++;
-}
-for (j = 0; j < start; j++)
-{
-if (s[j] == 45)
-{ negatives++; }
-}
-for (j = end; j >= start; j--)
-{
-number = number + (s[j] - '0') * p;
-p = p * 10;
-}
-if (negatives % 2 != 0)
-{ number = -number; }
-return (number);
+	while (s[start] < 48 || s[start] > 57)
+	{
+	start++;
+		if (start == size)
+		{
+		return (number2);
+		}
+	}
+	end = start;
+	while (s[end + 1] > 47 && s[end + 1] < 58)
+	{
+	end++;
+	}
+	for (j = 0; j < start; j++)
+	{
+	if (s[j] == 45)
+	{ negatives++; }
+	}
+	for (j = end; j >= start; j--)
+	{
+	number = number + (s[j] - '0') * p;
+	if (p < 1000000000)
+	{ p = p * 10; }
+	}
+	number2 = number;
+	if (negatives % 2 != 0)
+	{ 
+	number2 = -number2;
+	}
+return (number2);
 }
