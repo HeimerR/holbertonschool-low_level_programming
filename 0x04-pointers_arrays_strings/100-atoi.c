918a7e3ca2ef23cd190284bf6c07d3f00f52f7b5
Heimer <732@holberton.com>
2019-02-20 01:29:28 +0000

    q10

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+  * _atoi- convert a string to an integer.
+  * @s: string input
+  * Return: int number
+**/
+int _atoi(char *s)
+{
+int size = 0, negatives = 0, p = 1;
+int j, number = 0, start = 0, end;
+while (s[size] != '\0')
+{
+size++;
+}
+while (s[start] < 48 || s[start] > 57)
+{
+start++;
+}
+end = start;
+while (s[end + 1] > 47 && s[end + 1] < 58)
+{
+end++;
+}
+for (j = 0; j < start; j++)
+{ 
+if (s[j] == 45) 
+{ negatives++; }
+}
+for (j = end; j >= start; j--)
+{
+number = number + (s[j] - '0') * p; 
+p = p * 10;
+}
+if (negatives % 2 != 0)
+{ number = -number; }
+return (number);
+}
