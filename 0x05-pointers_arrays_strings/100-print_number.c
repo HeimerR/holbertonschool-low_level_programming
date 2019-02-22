fec3eef0fe4da04794fe95e712611503a3c5c63a
Heimer <732@holberton.com>
2019-02-22 01:45:06 +0000

    q9

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * print_number - prints number with _putchar
+  * @n: input number
+  * Return: no return
+ **/
+void print_number(int n)
+{
+	unsigned int number;
+
+	number = n;
+	if (n < 0)
+	{
+	_putchar('-');
+	number = -n;
+	}
+	if (number / 10 != 0)
+	{
+	print_number(number / 10);
+	}
+	_putchar((number % 10) + '0');
+}
