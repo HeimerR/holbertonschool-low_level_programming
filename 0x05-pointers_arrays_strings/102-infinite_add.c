60d804ccf3be62f40e632d10abf2c11d5b05156f
Heimer <732@holberton.com>
2019-02-22 03:40:00 +0000

    q12 test

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * nfinite_add - adds 2 numbers
+  * @n1: number 1
+  * @n2: number 2
+  * @size_r: buffer size
+  * @r: buffer
+  * Return: string encoded
+ **/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+while (
+return (n);
+}
