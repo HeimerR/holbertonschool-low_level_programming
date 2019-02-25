416bab629bd9ec4dd5d813eb4ba523234ba654a6
Heimer <732@holberton.com>
2019-02-25 20:37:01 +0000

    q4

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _strpbrk - gets the length of a prefix substring.
+* @s: input array
+* @accept: bytes to accept
+* Return: pointer
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j = 0;
+char *aux;
+aux = s;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (aux);
+}
+}
+aux++;
+}
+return (aux);
+}
