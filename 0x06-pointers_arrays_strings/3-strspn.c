49b0187b2bfec77f4a830b63b672fe0b8a1ca314
Heimer <732@holberton.com>
2019-02-25 20:08:03 +0000

    q3

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -7,23 +7,18 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-int j, i = 0;
-unsigned int count = 0;
-char *aux;
-aux = accept;
-while (aux[i] != '\0')
+int j, i = 0, count = 0;
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; s[j] != '\0'; j++)
-{
-if (s[j] == aux[i])
-{
-count++;
-aux++;
-i = -1;
-break;
+	if (accept[j] == '\0')
+	{ return (count); }
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[i] == accept[j])
+	{ count++;
+	break;
+	}
+	}
 }
-}
-i++;
-}
-return (count + 1);
+return (count);
 }
