605f692017261c41f82ad3cd475828a0fe02eb01
Heimer <732@holberton.com>
2019-02-28 03:51:51 +0000

    q7

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+/**
+  * _strlen_recursion - lenght
+  * @s: char
+  * Return: lenght
+**/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
+/**
+* comp - compares
+* @s: char
+* @start: start point
+* @end: end point
+* Return: boolean
+**/
+
+int comp(char *s, int start, int end)
+{
+if (start == end)
+{
+return (1);
+}
+if (s[start] != s[end])
+return (0);
+if (end < start)
+return (comp(s, start - 1, end  + 1));
+return (1);
+}
+/**
+* is_palindrome - returns 1 if is palindrome
+* @s: char
+* Return: boolean
+*/
+int is_palindrome(char *s)
+{
+char *aux;
+int size;
+int start = 0;
+int end;
+aux = s;
+size = _strlen_recursion(aux);
+end = size - 1;
+return (comp(s, start, end));
+}
+
