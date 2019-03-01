0eb6db76f8b0f221b3503dee66476059ee0d99b9
Heimer <732@holberton.com>
2019-03-01 17:25:11 +0000

    q0

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints its name, followed by a new line.
+* @argc: - # of parameters
+* @argv: - array of parameters
+* Return: Always 0.
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
