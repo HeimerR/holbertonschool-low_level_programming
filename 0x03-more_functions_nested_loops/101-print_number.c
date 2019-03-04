a313cefac102a162feaa69e7f7f78b14b85df97c
HeimerR <ing.heimer.rojas@gmail.com>
2019-03-04 03:15:28 +0000

    update q12 print number

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
   * print_number - prints numbers
   * @n: input number
@@ -6,30 +7,35 @@
 **/
 void print_number(int n)
 {
-	
+	unsigned int m = n, aux = n;
+	int i, x, power = 1, digits2 = 1, digits = 1;
+
 	if (n < 0)
-	{_putchar('-'); 
+	{ _putchar('-');
 	n = -n;
+	m = n;
+	aux = n; }
+	if (m > 0)
+	{
+		while (aux / 10 > 0)
+		{
+		aux = aux / 10;
+		digits++;
+		}
 	}
-	if(n < 10)
-	{ _putchar(n + '0'); }
-  	if(n < 100 && n > 9)
-	{ _putchar(n/10 + '0' );
-	_putchar(n%10 + '0' ); }
-	if(n < 1000 && n > 99)
-	{ _putchar( n / 100 + '0' );
-	_putchar((n / 10) % 10 + '0');
-	_putchar(n % 10 + '0'); } 
-	if(n < 10000 && n > 999)
-	{ _putchar(n / 1000 + '0');
-	 _putchar((n / 100) % 10 + '0');
-	 _putchar((n % 100) / 10 + '0');
-	 _putchar(n % 10 + '0'); }
-	if(n < 100000 && n > 9999)
- 	{ _putchar(n / 10000 + '0');
-	_putchar((n / 1000) % 10 + '0');
-	_putchar((n / 100) % 10 + '0');
-	_putchar((n % 100) / 10 + '0');
-	_putchar(n % 10 + '0'); }
+	aux = m;
+	digits2 = digits;
+	for (x = 0; x < digits2; x++)
+	{
+	power = 1;
+		for (i = digits - 1; i > 0; i--)
+		{
+		aux = aux / 10;
+		power = power * 10;
+		}
+	_putchar(aux + '0');
+	aux = m % power;
+	digits--;
+	}
 }
 
