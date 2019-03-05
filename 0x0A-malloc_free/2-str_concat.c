f158577aba38c4ae61e4dd66ce64c6fb72091b92
Heimer <732@holberton.com>
2019-03-05 19:32:26 +0000

    q2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int i, size1 = 0, size2 = 0;
 
+	if (s1 == NULL)
+	{ s1 = ""; }
+	if (s2 == NULL)
+	{ s2 = ""; }
 	while (s1[size1] != '\0')
 	{
 	size1++;
