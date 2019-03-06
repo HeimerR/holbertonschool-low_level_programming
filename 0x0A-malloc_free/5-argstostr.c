8eb95e46a6e6a3befbedf0426b99c5d34fb641c3
Heimer <732@holberton.com>
2019-03-06 01:51:47 +0000

    q5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* argstostr - concatenates all the arguments of your program
+* @ac: size
+* @av: array arguments
+* Return: double pointer
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0, k = 0, count = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+	return (0);
+	while (i < ac)
+	{
+	k = 0;
+	while (av[i][k] != '\0')
+	{
+		count++;
+		k++;
+	}
+	i++;
+	}
+	s = malloc((count + ac) * sizeof(char));
+	if (s == '\0')
+	return (0);
+	i = 0;
+	count = 0;
+	while (i < ac)
+	{
+		k = 0;
+		while (av[i][k] != '\0')
+		{
+			s[count] = av[i][k];
+			k++;
+			count++;
+		}
+		s[count] = 10;
+		count++;
+		k++;
+		i++;
+	}
+return (s);
+}
