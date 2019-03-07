96991245fa67cd1ee23c92854a7570478f52579d
Heimer <732@holberton.com>
2019-03-07 19:16:07 +0000

    q4 final

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -14,11 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr != NULL)
 	clone = ptr;
 	else
-	{ clone = malloc(new_size);
-		if (ptr == NULL)
-		return (0);
-		free(ptr);
-	return (clone); }
+	{ return (malloc(new_size)); }
 	if (new_size == old_size)
 	return (ptr);
 	if (new_size == 0 && ptr != NULL)
