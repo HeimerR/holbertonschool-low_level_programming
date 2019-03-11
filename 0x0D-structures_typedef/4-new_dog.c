8f7b5cfe4c1b6e09caa60db9ebd19451dd6976de
Heimer <732@holberton.com>
2019-03-11 21:36:24 +0000

    q4 come back

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,32 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-#include <string.h>
-/**
-* _strcpy - copies the string pointed to
-* @dest: destination array
-* @src: input string
-* Return: string
- **/
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0, length = 0;
-
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-	length = i;
-	for (i = 0; i < length && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	for ( ; i <= length; i++)
-	{
-		dest[i] = '\0';
-	}
-	return (dest);
-}
 /**
 * new_dog - check the code for Holberton School students.
 * @name: name's dog
@@ -41,14 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-	new->name = malloc(sizeof(*name));
-	if (new == NULL)
-		return (NULL);
-	_strcpy(new->name, name);
+	new->name = name;
 	new->age = age;
-	new->owner = malloc(sizeof(*owner));
-	if (new == NULL)
-		return (NULL);
-	_strcpy(new->owner, owner);
+	new->owner = owner;
 return (new);
 }
