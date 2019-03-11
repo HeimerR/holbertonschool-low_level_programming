2a9403cc823b514c0b424ad85087ca25371e597b
Heimer <732@holberton.com>
2019-03-11 21:10:31 +0000

    q4 more mallocs

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 /**
 * new_dog - check the code for Holberton School students.
 * @name: name's dog
@@ -15,8 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-	new->name = name;
+	new->name = malloc(sizeof(*name));
+	strcpy(new->name, name);
 	new->age = age;
-	new->owner = owner;
+	new->owner = malloc(sizeof(*owner));
+	strcpy(new->owner, owner);
 return (new);
 }
