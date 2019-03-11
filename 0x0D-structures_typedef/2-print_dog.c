f3c8c0dd574fef119d1c18a41effe5aea140acb9
Heimer <732@holberton.com>
2019-03-11 20:00:22 +0000

    q2 structs added format

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,15 +10,15 @@ void print_dog(struct dog *d)
 if (d != NULL)
 {
 	if (d->name == NULL)
-	printf("Name:%p\n", d->name);
+	printf("Name: %p\n", d->name);
 	else
-	printf("Name:%s\n", (*d).name);
+	printf("Name: %s\n", (*d).name);
 	if (d->age == 0)
-	printf("Age:%f\n", d->age);
+	printf("Age: %f\n", d->age);
 	else
-	printf("Age:%f\n", (*d).age);
+	printf("Age: %f\n", (*d).age);
 	if (d->owner == NULL)
-	printf("Owner:%p\n", d->owner);
+	printf("Owner: %p\n", d->owner);
 	else
 	printf("Owner%s\n", (*d).owner);
 }
