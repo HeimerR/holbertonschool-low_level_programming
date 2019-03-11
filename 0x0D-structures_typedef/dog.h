bda31a822d23ef968036df7cac668d2d4b763bee
Heimer <732@holberton.com>
2019-03-11 17:03:22 +0000

    q0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+* struct dog - dogs info
+* @name: Dog's name
+* @age: Dog's age
+* @owner: Dog's owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
