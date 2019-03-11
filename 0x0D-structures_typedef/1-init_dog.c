ba842cd1e748a209324838868308b7aab22edfcd
Heimer <732@holberton.com>
2019-03-11 18:28:49 +0000

    q1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+* init_dog - initializa struct dog
+* @d: struct type to initialize
+* @name: Dog's name
+* @age: Dog's age
+* @owner: Dog's owner
+*
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
