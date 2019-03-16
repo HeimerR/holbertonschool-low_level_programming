70998b8aa146e9f0ff2dbc600bc1bf01ac499139
Heimer <732@holberton.com>
2019-03-16 21:43:44 +0000

    q3 finally

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,82 +2,50 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
-* print_c - print char
-*
-* @s: va_list
-*/
-void print_c(va_list s)
-{
-	printf("%c", va_arg(s, int));
-}
-/**
-* print_i - print integer
-*
-* @s: va_list
-*/
-void print_i(va_list s)
-{
-	printf("%i", va_arg(s, int));
-}
-/**
-* print_f - print float
-*
-* @s: va_list
-*/
-void print_f(va_list s)
-{
-	printf("%f", va_arg(s, double));
-}
-/**
-* print_s - print string
-*
-* @s: va_list
-*/
-void print_s(va_list s)
-{
-
-	printf("%s", va_arg(s, char*));
-}
-
-/**
 * print_all - print char, integer, float and string
 *
 * @format: format
 */
 void print_all(const char * const format, ...)
 {
-
 	va_list list1;
-	unsigned int i = 0, j = 0;
-	int start = 0;
-
-	my_fmt fmts[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{'\0', '\0'}
-	};
+	unsigned int j = 0, start = 0;
+	char *p;
 
 	va_start(list1, format);
 	while (format[j] != '\0')
 	{
-		i = 0;
-		while (fmts[i].f != '\0')
-		{
-			if (*(fmts[i].f) == format[j])
-			{
-				switch (start)
-				{
-					case 1:
-					printf(", ");
-				}
-				fmts[i].p(list1);
-				start = 1;
-			}
-			i++;
-		}
+		switch (format[j])
+		{ case 'c':
+			switch (start)
+			{ case 1: printf(", ");	}
+			start = 1;
+			printf("%c", va_arg(list1, int));
+			break;
+		case 'i':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%i", va_arg(list1, int));
+			break;
+		case 'f':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%f", va_arg(list1, double));
+			break;
+		case's':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			p = va_arg(list1, char*);
+			if (p)
+			{ printf("%s", p);
+			break; }
+			printf("%p", p);
+			break; }
 		j++;
 	}
 	printf("\n");
+	va_end(list1);
 }
