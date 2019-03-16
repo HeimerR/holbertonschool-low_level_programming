c51d283fae102f6eaea2f04377d2081db71962c1
Heimer <732@holberton.com>
2019-03-16 19:49:59 +0000

    q3 header fixed

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -18,4 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _strlen_recursion(char *s);
+void print_c(va_list s);
+void print_i(va_list s);
+void print_f(va_list s);
+void print_s(va_list s);
 #endif
