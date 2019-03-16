da90f9afc7322807fa22029bf595bbc17ee0011e
Heimer <732@holberton.com>
2019-03-16 21:46:51 +0000

    this it it

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 	char *p;
 
 	va_start(list1, format);
-	while (format[j] != '\0')
+	while (format[j] != '\0' && format != '\0')
 	{
 		switch (format[j])
 		{ case 'c':
