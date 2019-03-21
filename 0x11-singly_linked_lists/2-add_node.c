cd3668156fe2fee99aff733336973cb4689b1137
Heimer <732@holberton.com>
2019-03-21 18:56:12 +0000

    [task2]

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * *add_node - adds new nodes to list
+ * @head: address new head
+ * @str: string to add
+ * Return: head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == 0)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LIST_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,5 +20,6 @@ typedef struct list_s
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
 
