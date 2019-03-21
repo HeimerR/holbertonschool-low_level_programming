2721bd22586b27d648b125eedb6d59a7e86f3f87
Heimer <732@holberton.com>
2019-03-21 22:36:27 +0000

    [task5]

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * first - functions firt
+ *
+ * Return: no return
+ */
+void __attribute__ ((constructor)) first(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
