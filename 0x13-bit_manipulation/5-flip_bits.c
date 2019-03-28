181bb71d951f5c6b7df6c87b99bca4bebc12e0b6
Heimer <732@holberton.com>
2019-03-28 19:56:39 +0000

    q5 extreme case

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -9,12 +9,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i, nflips = 0;
 
-	for (i = 1; i < 32; i++)
+	for (i = 0; i < 32; i++)
 	{
-	if ((m & 1) != (n & 1))
-		nflips += 1;
-	n = n >> 1;
-	m = m >> 1;
+		if ((m & 1) != (n & 1))
+			nflips += 1;
+		n = n >> 1;
+		m = m >> 1;
 	}
 return (nflips);
 }
