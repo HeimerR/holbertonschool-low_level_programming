bab95748589db680acdef4c01c058f25fe2100fc
Heimer <732@holberton.com>
2019-03-28 17:23:44 +0000

    q2

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number decimal
+ * @index: position to return
+ * Return: bit in position index.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	bit = (n >> index);
+	if (bit == 0)
+	return (-1);
+	return (bit & 1);
+}
