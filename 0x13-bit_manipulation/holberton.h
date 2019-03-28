9b0562c810888e50888049968f179a91c7a810c5
Heimer <732@holberton.com>
2019-03-28 19:08:55 +0000

    holberton.h

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -7,4 +7,5 @@ unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
