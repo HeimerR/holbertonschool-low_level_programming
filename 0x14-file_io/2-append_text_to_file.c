f8d4d77f060714e80391b263bcfc1f50ec632150
Heimer <732@holberton.com>
2019-04-02 19:39:12 +0000

    q2 betty style

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -16,9 +16,9 @@ int _strlen(char *s)
 	return (i);
 }
 /**
-* create_file - check the code for Holberton School students.
-* @filename: file to create.
-* @text_content: info to write into the file.
+* append_text_to_file - appends text at the end of a file.
+* @filename: file to append.
+* @text_content: info to append into the file.
 * Return: 1 on success, -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
