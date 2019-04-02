e43ef65f344ecda3db7082f67fba5bbd066dc486
Heimer <732@holberton.com>
2019-04-02 18:11:53 +0000

    q1

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
 
