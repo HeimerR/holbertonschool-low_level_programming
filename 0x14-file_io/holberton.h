3c9dab5f53ed19f0f561c39ba9b4db23739920ab
Heimer <732@holberton.com>
2019-04-02 19:36:57 +0000

    q2

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
