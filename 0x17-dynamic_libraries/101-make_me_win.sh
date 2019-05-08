b45a06baa86e6a0677aa96c8d7e2e5517f663270
Heimer <732@holbertonschool.com>
2019-05-08 22:08:03 +0000

    task advanced real attempt

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Congratulations, you win the Jackpot!"
-export LD_PRELOAD=$PWD/rand.so
+wget -P /tmp/ https://github.com/HeimerR/holbertonschool-low_level_programming/blob/master/0x17-dynamic_libraries/liball.so 
+export LD_PRELOAD=/tmp/liball.so
