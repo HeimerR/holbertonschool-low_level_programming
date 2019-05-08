3a114146bbde4b962475909b48c14edf17dcf736
Heimer <732@holbertonschool.com>
2019-05-08 22:17:04 +0000

    task advanced url fixed

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/HeimerR/holbertonschool-low_level_programming/blob/master/0x17-dynamic_libraries/liball.so 
+wget -P /tmp/ https://github.com/HeimerR/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/liball.so 
 export LD_PRELOAD=/tmp/liball.so
