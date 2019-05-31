29b22a452f3c261034b2597057971eacf50c15dc
Heimer <732@holbertonschool.com>
2019-05-31 17:54:07 +0000

    task 3 null conditions added

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -8,14 +8,20 @@
 **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int hash;
 	unsigned long int index;
+	unsigned long int hash;
 	hash_node_t *node, *aux;
 	char *val, *k;
 
+	if (!ht)
+		return (0);
+	if (!key)
+		return (0);
+	if (!value)
+		return (0);
 	if (key[0] == '\0')
 		return (0);
-	hash = hash_djb2((const unsigned char *)key);
+	hash = hash_djb2((unsigned char *)key);
 	index = hash % ht->size;
 	val = strdup(value);
 	aux = ht->array[index];
@@ -32,6 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 	k = strdup(key);
 	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
 	node->key = k;
 	node->value = val;
 	node->next = ht->array[index];
