6bb2a40aa7293934b3961b9992564e7c54c113f1
Heimer <732@holbertonschool.com>
2019-05-31 17:07:26 +0000

    task 0 null condition added

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,14 +10,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
 	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = NULL;
 	}
 	h_table = malloc(sizeof(hash_table_t));
+	if (h_table == NULL)
+		return (NULL);
 	h_table->array = array;
 	h_table->size = size;
 	return (h_table);
 }
-
