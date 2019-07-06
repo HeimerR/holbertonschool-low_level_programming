111b68ff3e6462f40f5c8b2f3fd18ed57c0b6dcf
HeimerR <732@holbertonschool.com>
2019-07-06 00:36:20 +0000

    task 100 againnn

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,19 +7,23 @@
 def island_perimeter(grid):
     sum_total1 = 0
     sum_total2 = 0
-    flag = 0
-    flag2 = 1
+    up_line = [0] * len(grid[0])
+    grid.insert(0, up_line)
+    grid.append(up_line)
+    for line in grid:
+        line.insert(0, 0)
+        line.append(0)
     for i in range(len(grid) - 1):
         sum_aux1 = sum(grid[i])
         sum_aux2 = sum(grid[i + 1])
         rest_aux = sum_aux2 - sum_aux1
         sum_total1 += abs(rest_aux)
-        for j in range(len(grid[0])):
-            if grid[i][j] != flag:
-                if grid[i + 1][j - flag2] != 1:
-                    sum_total2 += 1
-                flag2 = flag
-                flag = grid[i][j]
-        flag = 0
-        flag2 = 1
-    return sum_total1 + sum_total2 + 1
+    grid_t = list(zip(*grid))
+    sum_total2 = 0
+    for k in range(len(grid_t) - 1):
+        sum_aux1 = sum(grid_t[k])
+        sum_aux2 = sum(grid_t[k + 1])
+        rest_aux = sum_aux2 - sum_aux1
+        sum_total2 += abs(rest_aux)
+
+    return sum_total1 + sum_total2
