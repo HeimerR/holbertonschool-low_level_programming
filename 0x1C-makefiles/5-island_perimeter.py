0c98e367c0a1730efc55818cf37688782b4a61a2
HeimerR <732@holbertonschool.com>
2019-07-06 02:14:50 +0000

    task 100 final

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,20 +10,18 @@ def island_perimeter(grid):
     up_line = [0] * len(grid[0])
     grid.insert(0, up_line)
     grid.append(up_line)
-    for line in grid:
-        line.insert(0, 0)
-        line.append(0)
-    for i in range(len(grid) - 1):
-        sum_aux1 = sum(grid[i])
-        sum_aux2 = sum(grid[i + 1])
-        rest_aux = sum_aux2 - sum_aux1
-        sum_total1 += abs(rest_aux)
-    grid_t = list(zip(*grid))
-    sum_total2 = 0
-    for k in range(len(grid_t) - 1):
-        sum_aux1 = sum(grid_t[k])
-        sum_aux2 = sum(grid_t[k + 1])
-        rest_aux = sum_aux2 - sum_aux1
-        sum_total2 += abs(rest_aux)
+    grid_t = [list(i) for i in zip(*grid)]
+    grid_t.insert(0, up_line)
+    grid_t.append(up_line)
+    grid_t2 = [list(i) for i in zip(*grid_t)]
+    for i in range(len(grid_t)):
+        for j in range(len(grid_t[0])):
+            if grid_t[i][j] == 1 and grid_t[i][j - 1] == 0:
+                sum_total1 += 2
+
+    for i in range(len(grid_t2)):
+        for j in range(len(grid_t2[0])):
+            if grid_t2[i][j] == 1 and grid_t2[i][j - 1] == 0:
+                sum_total2 += 2
 
     return sum_total1 + sum_total2
