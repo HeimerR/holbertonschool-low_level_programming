ce3032e71b337b142604f4dd6f233655a06cb8cc
HeimerR <ing.heimer.rojas@gmail.com>
2020-05-24 01:17:08 +0000

    skip list added

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers.
+ * @list: head list pointer
+ * @value: value to search
+ *
+ * Return: pointer to the value or NULL
+**/
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *skip, *skip_prev;
+
+	if (!list)
+		return (NULL);
+	skip = list->express;
+	skip_prev = list;
+	while (skip)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", skip->index, skip->n);
+		if (skip->n >= value || !skip->express)
+		{
+			if (skip->n == value)
+				return (skip);
+			if (!skip->express && skip->n < value)
+			{
+				skip_prev = skip;
+				while (skip->next)
+					skip = skip->next;
+			}
+			printf("Value found between indexes [%lu] and [%lu]\n",
+					skip_prev->index, skip->index);
+			while (skip_prev)
+			{
+				printf("Value checked at index [%lu] = [%d]\n",
+						skip_prev->index, skip_prev->n);
+				if (skip_prev->n == value)
+					return (skip_prev);
+				if (skip_prev->n > value)
+					return (NULL);
+				skip_prev = skip_prev->next;
+			}
+			break;
+		}
+		skip_prev = skip;
+		skip = skip->express;
+	}
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -24,4 +24,23 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 listint_t *jump_list(listint_t *list, size_t size, int value);
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
